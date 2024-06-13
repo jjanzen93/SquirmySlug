e930ffa1a1ae98f7a70130433419ac1e67986946
jjanzen93 <132104176+jjanzen93@users.noreply.github.com>
2024-06-12 20:16:47 -0700

    Added MainMenu.js and main menu text assets

--- src/scenes/Level2.js
+++ src/scenes/Level2.js
@@ -20,18 +20,25 @@ class Level2 extends Phaser.Scene {
         // Second parameter: key for the tilesheet (from this.load.image in Load.js)
         this.tileset = this.map.addTilesetImage("gentle forest v01", "bright_tiles");
 
-        // Create a layer
         this.groundLayer = this.map.createLayer("Ground", this.tileset, 0, 0);
-        //this.groundLayer.setScale(1.666);
 
         // set up player avatar
         my.sprite.player = this.physics.add.sprite(game.config.width/2, this.map.heightInPixels*1, "slug", "Slug_0.png").setScale(1)
         my.sprite.player.body.setMaxVelocity(300);
-        this.blockerLayer = this.map.createLayer("Blockers", this.tileset, 0, 0);
-        //this.blockerLayer.setScale(1.666);
 
-        this.treeLayer = this.map.createLayer("Trees", this.tileset, 0, 0);
-        //this.treeLayer.setScale(1.666);
+        // make bush objects for sound and particle emission
+        this.bushes = this.map.createFromObjects("Bushes", {
+            name: "bush",
+            frame: 0,
+            visible: false
+        })
+
+        this.physics.world.enable(this.bushes, Phaser.Physics.Arcade.STATIC_BODY);
+
+        this.bushGroup = this.add.group(this.bushes);
+
+        this.blockerLayer = this.map.createLayer("Blockers", this.tileset, 0, 0);
+
 
         // Make it collidable
         this.blockerLayer.setCollisionByProperty({
@@ -39,6 +46,8 @@ class Level2 extends Phaser.Scene {
         });
 
         
+        this.treeLayer = this.map.createLayer("Trees", this.tileset, 0, 0);
+
 
         // Enable collision handling
         this.physics.add.collider(my.sprite.player, this.blockerLayer);
@@ -65,16 +74,7 @@ class Level2 extends Phaser.Scene {
         this.bush_sound = this.sound.add("foliage");
         this.bush_sound.setVolume(.4);
 
-        // make bush objects for sound and particle emission
-        this.bushes = this.map.createFromObjects("Bushes", {
-            name: "bush",
-            key: "bright_tiles",
-            frame: 26
-        })
 
-        this.physics.world.enable(this.bushes, Phaser.Physics.Arcade.STATIC_BODY);
-
-        this.bushGroup = this.add.group(this.bushes);
         
 
     }
@@ -82,28 +82,24 @@ class Level2 extends Phaser.Scene {
     update() {
         
         if(cursors.left.isDown) {
-            // TODO: have the player accelerate to the left
             my.sprite.player.body.setVelocityX(-200);
             my.sprite.player.body.setAccelerationY(-50);
             my.sprite.player.resetFlip();
             my.sprite.player.anims.play('walk', true);
 
         } else if(cursors.right.isDown) {
-            // TODO: have the player accelerate to the right
             my.sprite.player.body.setVelocityX(200);
             my.sprite.player.body.setAccelerationY(-50);
             my.sprite.player.setFlip(true, false);
             my.sprite.player.anims.play('walk', true);
 
         } else {
-            // TODO: set acceleration to 0 and have DRAG take over
             my.sprite.player.body.setAccelerationX(0);
             my.sprite.player.body.setVelocityX(0);
             //my.sprite.player.body.setDragX(this.DRAG);
         }
 
         // collision reset
-        // note that we need body.blocked rather than body.touching b/c the former applies to tilemap tiles and the latter to the "ground"
         if(my.sprite.player.body.blocked.up) {
             my.sprite.player.x = game.config.width/2;
             my.sprite.player.y = this.map.heightInPixels;
@@ -115,13 +111,10 @@ class Level2 extends Phaser.Scene {
             if (!this.inBush) {
                 this.bush_sound.play();
                 this.inBush = true;
+                my.sprite.player.body.setVelocityY(my.sprite.player.body.velocity.y* 3/4);
             }
         } else {
             this.inBush = false;
         }
-        //if(my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
-        //    // TODO: set a Y velocity to have the player "jump" upwards (negative Y direction)
-        //    my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
-        //}
     }
 }
