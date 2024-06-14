44b3ca34d81bf7e2a531e87bf178dbda984746ee
jjanzen93 <132104176+jjanzen93@users.noreply.github.com>
2024-06-14 00:04:06 -0700

    same as last commit

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -37,8 +37,8 @@ class Load extends Phaser.Scene {
 
         // Load sfx
         this.load.audio("main_music", "SlugSong.mp3");
-        this.load.audio("impact", "Retro_Impact_Punch.wav");
-        this.load.audio("foliage", "Retro_Impact_LoFi.wav");
+        this.load.audio("impact", "collision.wav");
+        this.load.audio("foliage", "foliage.wav");
         this.load.audio("swoosh", "Retro_Swooosh1.wav");
         this.load.audio("swoosh2", "Retro_Swooosh2.wav");
 
